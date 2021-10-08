b2805c8ea066f3a66100bc173ffd50a4af9710f4
DavidXie10 <davidxieli@gmail.com>
2021-10-07 23:53:14 -0600

    First functional plugin table

--- Laboratorio08/Laboratorio08/js/DataTable.js
+++ Laboratorio08/Laboratorio08/js/DataTable.js
@@ -5,22 +5,43 @@ class DataTable {
         this.context = context;
     }
 
+    appendTableCellsToRow(row, cells) {
+        for (let index = 0; index < cells.length; ++index) {
+            let cellElement = document.createElement("td");
+            let cellText = document.createTextNode(cells[index]);
+            cellElement.appendChild(cellText);
+            row.appendChild(cellElement);
+        }
+    }
+
     fillContext() {
         fetch(this.dataOrigin)
         .then(response => {
             return response.json();
         })
         .then(data => {
-            let htmlContent = '';
-            let row = '';
+            let table = document.createElement("table");
+            let tableBody = document.createElement("tbody");
+            let headerRow = document.createElement("tr");
+            this.appendTableCellsToRow(headerRow, ["Id", "Name", "Quantity", "Price"]);
+            tableBody.appendChild(headerRow);
+            
             for (const object of data) {
+                let row = document.createElement("tr");
+                let cells = []
                 for (const key in object) {
-                    row += `${object[key]}`
+                    cells.push(`${object[key]}`);
                 }
-                htmlContent += `<p>${row}</p><br/>`;
-                row = '';
+                this.appendTableCellsToRow(row, cells);
+                tableBody.append(row);
             }
-            this.context.innerHTML = htmlContent;
+            table.setAttribute("id", "productsTable");
+            table.appendChild(tableBody);
+            this.context.appendChild(table);
+
+            $(document).ready(function () {
+                $('#productsTable').DataTable();
+            });
         })
         .catch(error => {
             console.log(error);
